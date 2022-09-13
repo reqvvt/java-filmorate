e8ddff12bef5c85ef5f0a1afdfffa94c9d743eaa
Evgeniy <gekasan4@yandex.ru>
2022-09-13 11:52:27 +0300

    дб

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO mpa(rating_id, rating_value) VALUES (1, 'G');
-INSERT INTO mpa(rating_id, rating_value) VALUES (2, 'PG');
-INSERT INTO mpa(rating_id, rating_value) VALUES (3, 'PG-13');
-INSERT INTO mpa(rating_id, rating_value) VALUES (4, 'R');
-INSERT INTO mpa(rating_id, rating_value) VALUES (5, 'NC-17');
+INSERT INTO MPA(RATING_ID, RATING_VALUE) VALUES (1, 'G');
+INSERT INTO MPA(RATING_ID, RATING_VALUE) VALUES (2, 'PG');
+INSERT INTO MPA(RATING_ID, RATING_VALUE) VALUES (3, 'PG-13');
+INSERT INTO MPA(RATING_ID, RATING_VALUE) VALUES (4, 'R');
+INSERT INTO MPA(RATING_ID, RATING_VALUE) VALUES (5, 'NC-17');
 
-INSERT INTO genre(genre_id, genre) VALUES (1, 'Комедия');
-INSERT INTO genre(genre_id, genre) VALUES (2, 'Драма');
-INSERT INTO genre(genre_id, genre) VALUES (3, 'Мультфильм');
-INSERT INTO genre(genre_id, genre) VALUES (4, 'Триллер');
-INSERT INTO genre(genre_id, genre) VALUES (5, 'Документальный');
-INSERT INTO genre(genre_id, genre) VALUES (6, 'Боевик');
+INSERT INTO GENRES(GENRE_ID, GENRE) VALUES (1, 'Комедия');
+INSERT INTO GENRES(GENRE_ID, GENRE) VALUES (2, 'Драма');
+INSERT INTO GENRES(GENRE_ID, GENRE) VALUES (3, 'Мультфильм');
+INSERT INTO GENRES(GENRE_ID, GENRE) VALUES (4, 'Триллер');
+INSERT INTO GENRES(GENRE_ID, GENRE) VALUES (5, 'Документальный');
+INSERT INTO GENRES(GENRE_ID, GENRE) VALUES (6, 'Боевик');
 
